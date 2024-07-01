77d48a600b717425297684d6697ed88d104385ac
stae01 <166432863+stae01@users.noreply.github.com>
2024-06-30 22:17:05 -0700

    coms

--- JuastappPersistencia/src/main/java/collection/Addres.java
+++ JuastappPersistencia/src/main/java/collection/Addres.java
@@ -9,6 +9,12 @@ package collection;
  * @author PC Gamer
  */
 public class Addres {
+    
+    
+    /**
+     * Creación de las variables de clase
+     * 
+     */
     private String street;
     private String city;
 //    private String state;
@@ -16,9 +22,25 @@ public class Addres {
     private String country;
     private String hNumber;
 
+    /**
+     * Método constructor por omisión
+     * 
+     */
     public Addres() {
     }
 
+    /**
+     * 
+     * Método constructor que inicializa las variables de clase
+     * 
+     * @param street calle del usuario
+     * @param city ciudad del usuario
+     * @param state estado del usuario
+     * @param zipCode código postal del usuario
+     * @param country pais del usuario
+     * @param hNumber número de casa del usuario
+     * 
+     */
     public Addres(String street, String city, String state, String zipCode, String country,String hNumber) {
         this.street = street;
         this.city = city;
@@ -28,18 +50,46 @@ public class Addres {
         this.country = country;
     }
 
+    
+    /**
+     * 
+     * Método que obtiene la 
+     * calle del usuario
+     * 
+     * @return regresa la calle
+     * 
+     */
     public String getStreet() {
         return street;
     }
 
+    /**
+     * 
+     * Asigna el valor ala calle del usuario
+     * 
+     * @param street 
+     * 
+     */
     public void setStreet(String street) {
         this.street = street;
     }
 
+    /**
+     * 
+     * Método que obtiene la ciudad del usuario
+     * 
+     * @return regresa la ciudad del usuario
+     * 
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Método para asginar una ciudad 
+     * @param city ciudad a asignar
+     * 
+     */
     public void setCity(String city) {
         this.city = city;
     }
@@ -52,10 +102,24 @@ public class Addres {
 //        this.state = state;
 //    }
 //
+    
+    /**
+     * Método para obtener el código postal
+     * 
+     * @return regresa el código postal
+     * 
+     */
     public String getZipCode() {
         return zipCode;
     }
 
+    /**
+     * 
+     * Método para asignar el valor del código postal
+     * 
+     * @param zipCode 
+     * 
+     */
     public void setZipCode(String zipCode) {
         this.zipCode = zipCode;
     }
@@ -64,19 +128,55 @@ public class Addres {
 //        return country;
 //    }
 
+    /**
+     * 
+     * Método para obtener el número de la casa
+     * del usuario
+     * 
+     * 
+     * @return regresa el número
+     * 
+     * 
+     */
     public String gethNumber() {
         return hNumber;
     }
+    
 
+    /**
+     * 
+     * Método para asignar un número de casa
+     * 
+     * @param hNumber número de casa a asignar
+     * 
+     * 
+     */
     public void sethNumber(String hNumber) {
         this.hNumber = hNumber;
     }
 
     
+    /**
+     * 
+     * Método para asignar un pais
+     * 
+     * @param country pais a asignar
+     * 
+     * 
+     */
     public void setCountry(String country) {
         this.country = country;
     }
 
+    
+    /**
+     * 
+     * Método para obtener el pais del usuario
+     * 
+     * 
+     * @return regresa el pais del usuario
+     * 
+     */
     public String getCountry() {
         return country;
     }
